a779cbc259ed9f7cc55e9b4df628f3327baaf908
lujan <maria.caimmi@davi git config --global user.name maria>
2021-11-01 08:47:42 -0300

    c#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+            Console.WriteLine("Hello mundx!");
+            Console.WriteLine("Hello mundx!");
+            
+int num = 39;
+double total = num - 5;
+Console.WriteLine("El total de la operación es" + total.ToString());
+double resto = num - 3;
+Console.WriteLine("El total de la operación es" + resto.ToString());
+double restito= num * 2;
+Console.WriteLine("El total de la operación es" + restito.ToString());
+
+string cadena1 = "7";
+string cadena2 = "7";
+string mostrarCaadena = cadena1 + cadena2;
+Console.WriteLine(mostrarCaadena);
